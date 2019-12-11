f3d5c3fae9e528ce3545eb3bcd51d42bd5d98fe1
김민호 <v.i.c@hanmail.net>
2019-12-11 21:37:09 +0900

    1. 로그인 페이지에서 아이디 찾기 버튼 링크 달아줌 2. MemberDAO().selectOne(memberId)에서 admin_yn컬럼명 isAdmin으로 잘못 적었던 거 수정 및 getString으로 가져와서 Boolean으로 형변환

--- src/member/model/dao/MemberDAO.java
+++ src/member/model/dao/MemberDAO.java
@@ -43,7 +43,7 @@ public class MemberDAO {
 				m.setZipcode(rset.getString("zipcode"));
 				m.setAddress(rset.getString("address"));
 				m.setEnrolldate(rset.getDate("enrollDate"));
-				m.setAdmin(rset.getBoolean("isAdmin"));
+				m.setAdmin(Boolean.parseBoolean(rset.getString("admin_yn")));
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
