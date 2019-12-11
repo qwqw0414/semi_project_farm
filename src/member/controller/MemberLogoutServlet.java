597413908e4e6a12d5b6206545c349be7c7a94b7
김민호 <v.i.c@hanmail.net>
2019-12-11 14:21:41 +0900

    1. 로그아웃 기능 구현 2. header.jsp에 로그인여부 확인할 수 있게 <% memberLoggedIn %>작성

---
+++ src/member/controller/MemberLogoutServlet.java
@@ -0,0 +1,40 @@
+package member.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class MemberLogoutServlet
+ */
+@WebServlet("/member/memberLogout")
+public class MemberLogoutServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+				//로그아웃 => 세션객체 무효화
+				HttpSession session = request.getSession(false);
+				if(session!=null) {
+					session.invalidate();
+				}
+				//Redirect
+				response.sendRedirect(request.getContextPath());
+	}
+	
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
