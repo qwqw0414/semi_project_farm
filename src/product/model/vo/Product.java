219ccc4c59b4c820323f9d52b327d4cf4962f5e7
장예찬 <xzdi0414@gmail.com>
2019-12-12 17:13:41 +0900

    product 수정 admin 폴더 생성

--- src/product/model/vo/Product.java
+++ src/product/model/vo/Product.java
@@ -5,7 +5,7 @@ import java.io.Serializable;
 import javax.servlet.http.HttpSessionBindingEvent;
 import javax.servlet.http.HttpSessionBindingListener;
 
-public class Product implements Serializable, HttpSessionBindingListener {
+public class Product implements Serializable{
 
 	private static final long serialVersionUID = 1L;
 
@@ -16,12 +16,14 @@ public class Product implements Serializable, HttpSessionBindingListener {
 	private int price;
 	private double discount;
 	private int stock;
+	private String photo;
 	
 	public Product() {
 		
 	}
-	
-	public Product(String pId, String category, String pName, String pInfo, int price, double discount, int stock) {
+
+	public Product(String pId, String category, String pName, String pInfo, int price, double discount, int stock,
+			String photo) {
 		this.pId = pId;
 		this.category = category;
 		this.pName = pName;
@@ -29,6 +31,13 @@ public class Product implements Serializable, HttpSessionBindingListener {
 		this.price = price;
 		this.discount = discount;
 		this.stock = stock;
+		this.photo = photo;
+	}
+
+	@Override
+	public String toString() {
+		return "Product [pId=" + pId + ", category=" + category + ", pName=" + pName + ", pInfo=" + pInfo + ", price="
+				+ price + ", discount=" + discount + ", stock=" + stock + ", photo=" + photo + "]";
 	}
 
 	public String getpId() {
@@ -87,47 +96,16 @@ public class Product implements Serializable, HttpSessionBindingListener {
 		this.stock = stock;
 	}
 
+	public String getPhoto() {
+		return photo;
+	}
+
+	public void setPhoto(String photo) {
+		this.photo = photo;
+	}
+
 	public static long getSerialversionuid() {
 		return serialVersionUID;
-	}
-
-	@Override
-	public void valueBound(HttpSessionBindingEvent event) {
-	}
-
-	@Override
-	public void valueUnbound(HttpSessionBindingEvent event) {
-	}
-
-	@Override
-	public String toString() {
-		return "Product [pId=" + pId + ", category=" + category + ", pName=" + pName + ", pInfo=" + pInfo + ", price="
-				+ price + ", discount=" + discount + ", stock=" + stock + "]";
-	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((pId == null) ? 0 : pId.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Product other = (Product) obj;
-		if (pId == null) {
-			if (other.pId != null)
-				return false;
-		} else if (!pId.equals(other.pId))
-			return false;
-		return true;
 	}
 	
 	
